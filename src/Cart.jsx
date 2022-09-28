58b5b563dd6ebd9c1fabc8c1ee9b9f683f8dda27
Mikhail <mikhail86@icloud.com>
2022-09-28 17:25:02 +0100

    008 - Create a cart component

---
+++ src/Cart.jsx
@@ -0,0 +1,12 @@
+const Cart = () => {
+  return (
+    <div className="cart-container">
+      <h4 className="cart-title">Cart</h4>
+      <div className="cart-product">
+        <p className="cart-text">Your Cart is Empty</p>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "./Cart";
 import menu from "/images/menu.svg";
 import logo from "/images/logo.svg";
 import menuCart from "/images/menu-cart.svg";
 import avatar from "/images/avatar.png";
 
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+
   return (
     <header className="header">
       <img src={menu} alt="menu icon" className="header-menu-icon" />
       <img src={logo} alt="company logo" className="header-company-logo" />
-      <img src={menuCart} alt="cart icon" className="header-menu-cart" />
+      <Cart />
+
+      <div>
+        <img src={menuCart} alt="cart icon" className="header-menu-cart" />
+      </div>
       <img src={avatar} alt="avatar" className="header-avatar" />
     </header>
   );
