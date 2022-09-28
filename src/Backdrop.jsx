ee5e72fa60b692a6a5b0210135e33be6e485f2c8
Mikhail <mikhail86@icloud.com>
2022-09-28 22:32:27 +0100

    009/10/11 created and styled the backdrop, styled cart and created functionality for cart and backdrop

---
+++ src/Backdrop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Backdrop = (props) => {
+  return <div className="backdrop" onClick={props.onClick} />;
+};
+
+export default Backdrop;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,31 @@
 import React, { useState } from "react";
 import Cart from "./Cart";
+import Backdrop from "./Backdrop";
 import menu from "/images/menu.svg";
 import logo from "/images/logo.svg";
 import menuCart from "/images/menu-cart.svg";
 import avatar from "/images/avatar.png";
 
 const Header = () => {
-  const [showCart, setShowCart] = useState(false);
+  const [cartIsOpen, setCartIsOpen] = useState(false);
+
+  const openCart = () => {
+    setCartIsOpen((prevState) => !prevState);
+  };
+
+  const CloseCart = () => {
+    setCartIsOpen((prevState) => !prevState);
+  };
 
   return (
     <header className="header">
       <img src={menu} alt="menu icon" className="header-menu-icon" />
       <img src={logo} alt="company logo" className="header-company-logo" />
-      <Cart />
-
-      <div>
+      <div onClick={openCart}>
         <img src={menuCart} alt="cart icon" className="header-menu-cart" />
       </div>
+      {cartIsOpen && <Cart />}
+      {cartIsOpen && <Backdrop onClick={CloseCart} />}
       <img src={avatar} alt="avatar" className="header-avatar" />
     </header>
   );
